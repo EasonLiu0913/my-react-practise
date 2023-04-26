eb5dc76038d5d10d4dc015d63ec34b727536699c
Eason <bettercalleason@gmail.com>
2023-04-26 18:15:23 +0800

    新增輪播牆效果

--- myWebsite/src/pages/Home/index.jsx
+++ myWebsite/src/pages/Home/index.jsx
@@ -1,10 +1,100 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Container } from './styled';
+import fontawesome from '@fortawesome/fontawesome';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckSquare, faCoffee } from '@fortawesome/fontawesome-free-solid';
 
 export default function Home() {
+    const [carouselIndex, setCarouselIndex] = useState(0);
+    const [carouselMoveX, setCarouselMoveX] = useState(0);
+    let carousel = useRef(null);
+
+    useEffect(() => {
+        let myInterval = null;
+
+        function startInterval() {
+            myInterval = setInterval(() => {
+                setCarouselIndex((carouselIndex) => (carouselIndex + 1) % 5);
+            }, 3000);
+        }
+
+        function mouseEnter() {
+            clearInterval(myInterval);
+        }
+
+        function mouseLeave() {
+            startInterval();
+        }
+
+        carousel.current.addEventListener('mouseenter', mouseEnter);
+        carousel.current.addEventListener('mouseleave', mouseLeave);
+
+        startInterval();
+        return () => {
+            clearInterval(myInterval);
+
+            if (carousel.current) {
+                carousel.current.removeEventListener('mouseenter', mouseEnter);
+                carousel.current.removeEventListener('mouseleave', mouseLeave);
+            }
+        };
+    }, []);
+
+    function handlePrevPage() {
+        setCarouselIndex(carouselIndex - 1 < 0 ? 4 : carouselIndex - 1);
+    }
+
+    function handleNextPage() {
+        setCarouselIndex(carouselIndex + 1 > 4 ? 0 : carouselIndex + 1);
+    }
+
     return (
-        <Container>
-            <h1>Home</h1>
-        </Container>
+        <div
+            style={{
+                background: 'linear-gradient(135deg,lightblue,lightpink)',
+                minHeight: '100vh',
+            }}
+        >
+            <Container>
+                <div className="carousel" ref={carousel}>
+                    <ul
+                        className="img-list"
+                        style={{
+                            transform: `translateX(-${
+                                carouselIndex *
+                                    (carousel.current &&
+                                        carousel.current.offsetWidth) +
+                                2
+                            }px)`,
+                            transition: '1s',
+                        }}
+                    >
+                        <li>
+                            <img src="/carousel-imgs/1.jpg" alt="" />
+                        </li>
+                        <li>
+                            <img src="/carousel-imgs/2.jpg" alt="" />
+                        </li>
+                        <li>
+                            <img src="/carousel-imgs/3.jpg" alt="" />
+                        </li>
+                        <li>
+                            <img src="/carousel-imgs/4.jpg" alt="" />
+                        </li>
+                        <li>
+                            <img src="/carousel-imgs/5.jpg" alt="" />
+                        </li>
+                    </ul>
+
+                    <div className="prevPage" onClick={handlePrevPage}>
+                        <FontAwesomeIcon icon="fa-solid fa-arrow-left" />
+                    </div>
+
+                    <div className="nextPage" onClick={handleNextPage}>
+                        <FontAwesomeIcon icon="fa-solid fa-arrow-right" />
+                    </div>
+                </div>
+            </Container>
+        </div>
     );
 }
--- myWebsite/src/pages/Home/styled.jsx
+++ myWebsite/src/pages/Home/styled.jsx
@@ -4,4 +4,51 @@ export const Container = styled.div`
     width: 80%;
     margin: auto;
     padding-top: 100px;
+
+    @media screen and (max-width: 768px) {
+        width: 100%;
+        padding-top: 70px;
+    }
+
+    .carousel {
+        overflow: hidden;
+        position: relative;
+    }
+
+    .carousel ul.img-list {
+        list-style: none;
+        padding: 0;
+        display: flex;
+        width: 500%;
+    }
+
+    .carousel ul.img-list li {
+        flex: 1 0 0;
+    }
+
+    .carousel ul.img-list img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+
+    .prevPage,
+    .nextPage {
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        width: 100px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background: #e0ffff43;
+    }
+
+    .prevPage {
+        left: 0;
+    }
+
+    .nextPage {
+        right: 0;
+    }
 `;
